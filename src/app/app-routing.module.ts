7559a66cee6a892c237b13a7ede8e0218a10c9c5
raspyweather <raspyweather@gmail.com>
2018-01-05 22:16:17 +0100

    registered temporary askQuestionComponent Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router"
 import { CreateQuestionComponent } from "./questions/createquestion/createquestion.component";
 import { CreateQuestionPageComponent } from "./pages/create-question-page/create-question-page.component";
-
+import { AskquestionComponent } from "./questions/askquestion/askquestion.component";
 const routes: Routes = [
   { path: 'createQuestionPage', component: CreateQuestionPageComponent },
+  { path: 'createQuestion', component: CreateQuestionComponent },
+  { path: 'askQuestion', component: AskquestionComponent },
+
 ];
 
 @NgModule({
